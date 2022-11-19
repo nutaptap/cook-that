a78040322298e5b44b40a0db9126066b4bc30ec6
nutaptap <nutaptap@gmail.com>
2022-11-20 00:50:09 +0100

    Added functionality to the filter buttons

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-/*--------------CHANGE TAGS---------------*/
-let search = "vegetarian, gluten-free";
+/*-------------CHANGE FILTERS-------------*/
+const buttons = document.getElementsByClassName("filters");
+let diet = "";
+
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    changeFilters(this.innerHTML);
+    makeActive(this.innerHTML);
+  });
+}
+
+function changeFilters(filter) {
+  term = filter.toLowerCase();
+  diet = term;
+  console.log(diet);
+}
+
+function makeActive(selected) {
+  for (i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerHTML === selected) {
+      if (buttons[i].classList.contains("active")) {
+        buttons[i].className = "filters";
+      } else {
+        buttons[i].className += " active";
+      }
+    }
+  }
+}
 
 /*------------GET A NEW RECIPE------------*/
 
@@ -12,12 +38,12 @@ recipeButton.addEventListener("click", function () {
 });
 
 function getOffset() {
-  offset = Math.floor(Math.random() * 50);
+  offset = Math.floor(Math.random() * 20);
 }
 
 async function getRecipe() {
   const res = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=32919dc2eb944e8482f7ed12fedfad71&number=1&maxReadyTime=20&addRecipeInformation=true&sort=popular&query=${search}&offset=${offset}`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=32919dc2eb944e8482f7ed12fedfad71&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${diet}&offset=${offset}`
   );
   res.json().then(function (res) {
     console.log(res);
