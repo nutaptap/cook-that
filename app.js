2ba1b580c217bc6c9cc0634b21013df1c081aaac
nutaptap <nutaptap@gmail.com>
2022-11-20 17:41:02 +0100

    Added the all button and can now remove filters

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
 /*-------------CHANGE FILTERS-------------*/
-const buttons = document.getElementsByClassName("filters");
-let diet = "";
+const buttons = document.getElementsByClassName("diet");
+let dietArray = [];
+let dietString = "";
 
 for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    changeFilters(this.innerHTML);
+    changeDiet(this.innerHTML);
     makeActive(this.innerHTML);
   });
 }
 
-function changeFilters(filter) {
-  term = filter.toLowerCase();
-  diet = term;
-  console.log(diet);
+function changeDiet(selection) {
+  diet = selection.toLowerCase();
+
+  if (dietArray.includes(diet)) {
+    const index = dietArray.indexOf(diet);
+    dietArray.splice(index, 1);
+  } else {
+    dietArray.push(diet);
+  }
+
+  dietString = dietArray.toString();
+  console.log(dietArray, dietString);
 }
 
 function makeActive(selected) {
+  isAll = false;
+  allButton.className = "";
   for (i = 0; i < buttons.length; i++) {
     if (buttons[i].innerHTML === selected) {
       if (buttons[i].classList.contains("active")) {
-        buttons[i].className = "filters";
+        buttons[i].className = "diet";
       } else {
         buttons[i].className += " active";
       }
     }
   }
+}
+
+/*---------------ALL BUTTON---------------*/
+const allButton = document.getElementById("All");
+let isAll = true;
+allButton.className = "active";
+
+allButton.addEventListener("click", function () {
+  changeAll();
+});
+
+function changeAll() {
+  isAll = true;
+  allButton.className = "active";
+  dietArray = [];
+  dietString = "";
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].className = "diet";
+  }
+  console.log(dietArray, dietString);
 }
 
 /*------------GET A NEW RECIPE------------*/
@@ -43,7 +74,7 @@ function getOffset() {
 
 async function getRecipe() {
   const res = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=32919dc2eb944e8482f7ed12fedfad71&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${diet}&offset=${offset}`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=32919dc2eb944e8482f7ed12fedfad71&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${dietString}&offset=${offset}`
   );
   res.json().then(function (res) {
     console.log(res);
