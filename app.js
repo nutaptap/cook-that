fb95bda875fd2817dcf015548eaa702ab6ffb160
nutaptap <nutaptap@gmail.com>
2022-12-29 10:17:39 +0100

    Changed the way filters work, implemented the new colors and fonts and the new nav bar

--- app.js
+++ app.js
@@ -2,61 +2,22 @@ const recipeCard = document.getElementById("recipe-card");
 recipeCard.classList.add("invisible");
 
 /*-------------CHANGE FILTERS-------------*/
-const buttons = document.getElementsByClassName("diet");
+
+const filters = document.getElementById("filters");
 let recipeData = {};
-let dietArray = [];
-let dietString = "";
+let filterString = "";
+let type = "";
 
-for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    changeDiet(this.innerHTML);
-    makeActive(this.innerHTML);
-  });
-}
-
-function changeDiet(selection) {
-  diet = selection.toLowerCase();
-
-  if (dietArray.includes(diet)) {
-    const index = dietArray.indexOf(diet);
-    dietArray.splice(index, 1);
-  } else {
-    dietArray.push(diet);
-  }
-
-  dietString = dietArray.toString();
-}
-
-function makeActive(selected) {
-  isAll = false;
-  allButton.className = "";
-  for (i = 0; i < buttons.length; i++) {
-    if (buttons[i].innerHTML === selected) {
-      if (buttons[i].classList.contains("active")) {
-        buttons[i].className = "diet";
-      } else {
-        buttons[i].className += " active";
-      }
-    }
-  }
-}
-
-/*---------------ALL BUTTON---------------*/
-const allButton = document.getElementById("All");
-let isAll = true;
-allButton.className = "active";
-
-allButton.addEventListener("click", function () {
-  changeAll();
+filters.addEventListener("change", function () {
+  changeFilter(this.value);
 });
 
-function changeAll() {
-  isAll = true;
-  allButton.className = "active";
-  dietArray = [];
-  dietString = "";
-  for (i = 0; i < buttons.length; i++) {
-    buttons[i].className = "diet";
+function changeFilter(selection) {
+  if (selection === "dessert") {
+    type = "dessert";
+  } else {
+    type = "";
+    filterString = selection;
   }
 }
 
@@ -71,18 +32,17 @@ recipeButton.addEventListener("click", function () {
 });
 
 function getOffset() {
-  offset = Math.floor(Math.random() * 20);
+  offset = Math.floor(Math.random() * 60);
 }
 
 async function getRecipe() {
   const res = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=918aa9352c7f4a8c890ce6775ff91479&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${dietString}&offset=${offset}`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=1cc62e8cd6f84ab3ba211ed0bb158210&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${filterString}&type=${type}&offset=${offset}`
   );
   res.json().then(function (res) {
     recipe = res.results;
     updateTitle(recipe[0].title);
     updateImage(recipe[0].image);
-    /* updateIngredients(recipe[0].analyzedInstructions[0].steps); */
     updateInstructions(recipe[0].analyzedInstructions[0].steps);
     getIngredients(recipe[0].id);
   });
@@ -99,7 +59,7 @@ async function getIngredients(id) {
 }
 
 /*-----------DISPLAY THE RECIPE-----------*/
-/* const r = document.querySelector(":root"); */
+
 const title = document.getElementById("title-display");
 const ingredients = document.getElementById("ingredients-display");
 const instructions = document.getElementById("instructions-display");
