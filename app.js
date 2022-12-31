3a88eaecbd7c7e5b551dc0602b0f13cb29b145b5
nutaptap <nutaptap@gmail.com>
2022-12-31 22:35:51 +0100

    Added back all the functionality and included more recipe information like nutritional values and cooking time

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const recipeCard = document.getElementById("recipe-card");
-recipeCard.classList.add("invisible");
+/* recipeCard.classList.add("invisible"); */
 
 /*-------------CHANGE FILTERS-------------*/
 
 const filters = document.getElementById("filters");
 let recipeData = {};
 let filterString = "";
-let type = "";
 
 filters.addEventListener("change", function () {
   changeFilter(this.value);
 });
 
 function changeFilter(selection) {
-  if (selection === "dessert") {
-    type = "dessert";
-  } else {
-    type = "";
-    filterString = selection;
-  }
+  type = "";
+  filterString = selection;
 }
 
 /*------------GET A NEW RECIPE------------*/
@@ -37,16 +32,20 @@ function getOffset() {
 
 async function getRecipe() {
   const res = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=1cc62e8cd6f84ab3ba211ed0bb158210&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&diet=${filterString}&type=${type}&offset=${offset}`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=1cc62e8cd6f84ab3ba211ed0bb158210&number=1&maxReadyTime=30&addRecipeInformation=true&sort=popular&addRecipeNutrition=true&diet=${filterString}&offset=${offset}`
   );
   res.json().then(function (res) {
+    console.log(res);
     recipe = res.results;
     updateTitle(recipe[0].title);
     updateImage(recipe[0].image);
     updateInstructions(recipe[0].analyzedInstructions[0].steps);
+    updateNutrition(recipe[0].nutrition);
+    updateTime(recipe[0].readyInMinutes);
+    updateTag();
     getIngredients(recipe[0].id);
   });
-  recipeCard.classList.remove("invisible");
+  /* recipeCard.classList.remove("invisible"); */
 }
 
 async function getIngredients(id) {
@@ -64,6 +63,13 @@ const title = document.getElementById("title-display");
 const ingredients = document.getElementById("ingredients-display");
 const instructions = document.getElementById("instructions-display");
 const image = document.getElementById("recipe-image");
+const tag = document.getElementById("recipe-tag");
+const calories = document.getElementById("calories");
+const protein = document.getElementById("protein");
+const carbs = document.getElementById("carbs");
+const fats = document.getElementById("fats");
+const fiber = document.getElementById("fiber");
+const time = document.getElementById("time");
 
 function updateTitle(recipeTitle) {
   title.innerText = recipeTitle;
@@ -73,10 +79,30 @@ function updateImage(recipeImage) {
   image.src = recipeImage;
 }
 
+function updateNutrition(nutrition) {
+  calories.innerText = nutrition.nutrients[0].amount;
+  protein.innerText = `${nutrition.nutrients[8].amount} ${nutrition.nutrients[8].unit}`;
+  carbs.innerText = `${nutrition.nutrients[3].amount} ${nutrition.nutrients[3].unit}`;
+  fats.innerText = `${nutrition.nutrients[1].amount} ${nutrition.nutrients[1].unit}`;
+  fiber.innerText = nutrition.nutrients[14].amount;
+}
+
+function updateTime(minutes) {
+  time.innerText = `${minutes}m`;
+}
+
+function updateTag() {
+  if (filterString === "") {
+    tag.innerText = "all";
+  } else {
+    tag.innerText = filterString;
+  }
+}
+
 function updateInstructions(recipeData) {
   instructions.innerHTML = "";
 
-  const newList = document.createElement("ol");
+  const newList = new DocumentFragment();
 
   for (i = 0; i < recipeData.length; i++) {
     let li = document.createElement("li");
@@ -95,7 +121,7 @@ function updateIngredients(recipeData) {
     ingredientsList.push(recipeData[i].original);
   }
 
-  const newList = document.createElement("ul");
+  const newList = new DocumentFragment();
 
   for (i = 0; i < ingredientsList.length; i++) {
     let li = document.createElement("li");
